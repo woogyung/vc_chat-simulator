3e1bd0c3272a3e85cd10fef55abd2b67b1e46784
laranhee <laranhee@gmail.com>
2017-11-23 00:17:50 +0900

    .editorconfig 생성 중간커밋

--- src/index.js
+++ src/index.js
@@ -6,21 +6,38 @@ import CHAT_DATA from './lib/chatHistory.json';
 // `CHAT_DATA`라는 변수에 채팅 히스토리가 담겨있습니다. 이 데이터를 이용하시면 됩니다.
 console.log(CHAT_DATA);
 
-function View() {
+(function () {
+  function View() {
     this.element = null;
-}
+  }
 
-function ChatContainerView(selector) {
+  function ChatContainerView(selector) {
     View.call(this);
 
     this.element = document.querySelector(selector);
-}
+  }
 
-ChatContainerView.prototype = Object.create(View.prototype);
-ChatContainerView.prototype.constructor = ChatContainerView;
+  ChatContainerView.prototype = Object.create(View.prototype);
+  ChatContainerView.prototype.constructor = ChatContainerView;
 
-ChatContainerView.prototype.addMessage = function addMessage(messageView) {
+  ChatContainerView.prototype.setMessage = function setMessage(messageView) {
     this.element.appendChild(messageView.element);
-};
+  };
+
+  function ChatItemView(message) {
+    View.call(this);
+
+    this.element = document.createElement('div');
+    this.element.className = 'chat';
+
+    if (message.payload.type === 'message') {
+      document.createElement('span');
+      message.payload.user.display_name
+    }
+    this.message = message;
+
+
+  }
+})();
 
 debugger;
