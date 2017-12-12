2d39ed05f07fcc82408f6a72d5a1bda81b6b7969
wonjae <dldnjswo19@gmail.com>
2017-12-12 16:32:58 +0900

    change in a better way

--- src/index.js
+++ src/index.js
@@ -6,55 +6,53 @@ import CHAT_DATA from './lib/chatHistory.json';
 // `CHAT_DATA`라는 변수에 채팅 히스토리가 담겨있습니다. 이 데이터를 이용하시면 됩니다.
 console.log(CHAT_DATA);
 
-// delta is time interval from start chat simulator
-// payload-type = [message, connect, update, delete, disconnect]
+// delta is chatTime interval from start chat simulator
+// payload-type = { message, connect, update, delete, disconnect }
 // payload-user = { id, user_name, display_name }
 // payload-message = { id, text }
 
-function ChatSimulator(CHAT_DATA) {
-  this.chatData = CHAT_DATA;
-  this.mainUser = new User(
-      {id: 1, user_name: 'taco', display_name: 'Taco Spolsky'});
-  this.users = {
-    1: this.mainUser,
-    2: new User({id: 2, user_name: 'chorizo', display_name: 'Chorizo'}),
-  };
-  this.messages = {};
-  this.chatView = document.querySelector('.chat-view');
-  this.time = 0;
+var users = {};
+var messages = {};
+var chatView = document.querySelector('.chat-view');
+
+function startChat(data) {
+  var chatTime = 0;
+  connectClient({id: 1, user_name: 'taco', display_name: 'Taco Spolsky'});
+  connectClient({id: 2, user_name: 'chorizo', display_name: 'Chorizo'});
+
+  data.forEach(function(packet) {
+    chatTime += packet.delta;
+    setTimeout(packetHandler.bind(null, packet.payload, chatTime), chatTime);
+  });
 }
 
-ChatSimulator.prototype.startChat = function() {
-  var self = this;
+function packetHandler(data, chatTime) {
 
-  this.chatData.forEach(function(packet) {
-    self.time += packet.delta;
-    self.packetHandler(packet);
-  });
+  switch (data.type) {
+    case 'message':
+      sendMessage(data.user, data.message, chatTime);
+      break;
+    case 'connect':
+      connectClient(data.user);
+      break;
+    case 'disconnect':
+      disconnectClient(data.user);
+      break;
+    case 'update':
+      update(data);
+      break;
+    case 'delete':
+      deleteMessage(data.message.id);
+      break;
+  }
+}
 
-};
-
-ChatSimulator.prototype.connectClient = function(user) {
-  this.users[user.id] = new User(user);
-  this.send(
-      this.createSystemMessage('\'' + user.display_name +
-          '\' connected!'),
-      this.time);
-};
-
-ChatSimulator.prototype.disconnectClient = function(user) {
-  delete this.users[user.id];
-  this.send(this.createSystemMessage('\'' + user.display_name +
-      '\' disconnected TT'),
-      this.time);
-};
-
-ChatSimulator.prototype.sendMessage = function(user, message) {
-  this.messages[message.id] = message.text;
+function sendMessage(user, message, chatTime) {
+  messages[message.id] = message.text;
 
   var container = document.createElement('div');
   container.className = 'container';
-  container.id = message.id;
+  container.dataset.id = message.id;
 
   var nickname = document.createElement('p');
   nickname.className = 'nickName';
@@ -63,100 +61,83 @@ ChatSimulator.prototype.sendMessage = function(user, message) {
   var text = document.createElement('p');
   text.innerHTML = message.text;
 
-  var time = document.createElement('span');
-  time.innerHTML = this.time;
-  time.className = 'time-right';
+  var timeText = document.createElement('span');
+  timeText.innerHTML = chatTime;
+  timeText.className = 'time-right';
 
-  if (user.user_name === this.mainUser.user_name) container.classList.add(
+  if (users[1] &&
+      user.user_name === users[1].user_name) container.classList.add(
       'darker');
 
   container.appendChild(nickname);
   container.appendChild(text);
-  container.appendChild(time);
+  container.appendChild(timeText);
 
-  this.send(container, this.time);
-};
+  send(container);
+}
 
-ChatSimulator.prototype.deleteMessage = function(id) {
-  var self = this;
-  setTimeout(function() {
-    self.updateMessage({id : id, text: 'message deleted'});
-  }, this.time);
+function send(element) {
+  chatView.appendChild(element);
+}
 
-  this.send(this.createSystemMessage('message id_' + id +
-      ' deleted!'), this.time);
-};
+function connectClient(user) {
+  users[user.id] = new User(user);
+  send(createSystemMessage('\'' + user.display_name + '\' connected!'));
+}
 
-ChatSimulator.prototype.createSystemMessage = function(message) {
+function createSystemMessage(message) {
   var systemMessage = document.createElement('div');
   systemMessage.className = 'system-message';
   systemMessage.innerHTML = message;
 
   return systemMessage;
-};
+}
 
-ChatSimulator.prototype.send = function(element, delta) {
-  var self = this;
-  setTimeout(function() {
-    self.chatView.appendChild(element);
-  }, delta);
-};
+function disconnectClient(user) {
+  delete users[user.id];
+  send(createSystemMessage('\'' + user.display_name + '\' disconnected TT'));
+}
 
-ChatSimulator.prototype.update = function(data) {
+function update(data) {
   var message = '';
   var cb, param;
 
   if (data.user) {
-    var target = this.users[data.user.id];
+    var target = users[data.user.id];
     var pastName = target.display_name;
 
-    cb = this.updateUserData;
+    cb = updateUserData;
     param = target;
     message = pastName + ' updates name - ' + data.user.display_name;
 
   } else if (data.message) {
 
-    cb = this.updateMessage;
+    cb = updateMessage;
     data.message.text += ' (edited)';
     param = data.message;
     message = 'update message id : ' + data.message.id;
   }
 
-  setTimeout(cb.bind(this, param), this.time);
-  this.send(this.createSystemMessage(message), this.time);
-};
+  cb(param);
+  send(createSystemMessage(message));
+}
 
-ChatSimulator.prototype.updateMessage = function(message) {
-  var container = document.getElementById(message.id.toString());
-  container.children[1].innerHTML = message.text;
-};
-
-ChatSimulator.prototype.updateUserData = function(target) {
+function updateUserData(target) {
   target.updateInfo(
       {user_name: target.user_name, display_name: target.display_name});
-};
+}
 
-ChatSimulator.prototype.packetHandler = function(packet) {
-  var payload = packet.payload;
+function updateMessage(message) {
+  var container = document.querySelector('[data-id="' + message.id.toString() +
+      '"]');
+  container.children[1].innerHTML = message.text;
+}
 
-  switch (payload.type) {
-    case 'message':
-      this.sendMessage(payload.user, payload.message);
-      break;
-    case 'connect':
-      this.connectClient(payload.user);
-      break;
-    case 'disconnect':
-      this.disconnectClient(payload.user);
-      break;
-    case 'update':
-      this.update(payload);
-      break;
-    case 'delete':
-      this.deleteMessage(payload.message.id);
-      break;
-  }
-};
+function deleteMessage(id) {
+
+  updateMessage({id: id, text: 'message deleted'});
+  send(createSystemMessage('message id_' + id + ' deleted!'));
+}
 
 // for connected-friend list
 function User(options) {
@@ -170,4 +151,4 @@ User.prototype.updateInfo = function(options) {
   this.display_name = options.display_name;
 };
 
-(new ChatSimulator(CHAT_DATA)).startChat();
+startChat(CHAT_DATA);
